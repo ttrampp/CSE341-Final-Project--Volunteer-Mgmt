ef3fd515361b78504046b1493fd748887c05b96b
Erik Burton <erikburton1@gmail.com>
2025-06-06 18:58:37 -0600

    Serve swagger.json file.

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+router.get('/swagger.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '../swagger.json'));
+});
 
 module.exports = router;
