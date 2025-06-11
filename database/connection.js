8390b617d08b34a97dc3107024273ee90ac2975c
Jake Preciado <135553746+jakepreciado@users.noreply.github.com>
2025-06-11 09:53:27 -0600

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,16 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const uri = process.env.MONGODB_URI;
-  const dbName = process.env.MONGODB_DB_NAME || 'volunteerDB';    //fallback
-
   if (!uri) {
     console.error("Missing MONGODB_URI in .env file");
     process.exit(1);
   }
-    await mongoose.connect(uri, {
-      dbName,
-    });
-    console.log(`MongoDB connected to ${dbName}`);
+
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
